ca2ff91f78c686613f4f81b9b3411892fa3a6451
spencer Reynolds <spencerreynolds88@gmail.com>
2022-01-29 14:43:56 -0600

    Initial Release

--- scripts/Wounds.js
+++ scripts/Wounds.js
@@ -17,8 +17,9 @@ class wounds {
         Hooks.on('createCombatant', wounds.createTracker);
 
         Hooks.on('updateCombat', wounds.checkCombatTrigger);
-        
+        Hooks.on("midi-qol.AttackRollComplete", injuries.critAttack)
         Hooks.on("preUpdateActor", wounds.preUpdateActor);
+        Hooks.on("deleteCombat", injuries.injuryAdjudication);
         //Hooks.on("deleteCombat", cleanupFlags)
     }
 
@@ -74,63 +75,81 @@ class wounds {
 
     static preUpdateActor(actor, update) {
         let hp = getProperty(update, "data.attributes.hp.value");
-        if (hp !== undefined && !actor.isNPC) {
+        if (hp <= 0 && !actor.isNPC) {
+            injuries.addInjuryToken(actor);
+        } else if (hp !== undefined && !actor.isNPC) {
             wounds.calculation(actor, update);
         }
     }
 
+
+
+    
     static calculation(actor, update) {
         let data = {
-            actor: actor,
-            actorData: actor.data,
-            updateData: update,
-            //assumes that PC only has one token
-            actorToken: actor.getActiveTokens()[0],
-            //currentWoundRisk: token.combatant.getFlag(MODULE.data.name, 'hasWoundRisk'),
-            actorHP: actor.data.data.attributes.hp.value,
-            actorMax: actor.data.data.attributes.hp.max,
+
             actorConMod: actor.data.data.abilities.con.mod,
-            updateHP: (hasProperty(update, "data.attributes.hp.value") ? update.data.attributes.hp.value : 0),
             hpChange: (actor.data.data.attributes.hp.value - (hasProperty(update, "data.attributes.hp.value") ? update.data.attributes.hp.value : actor.data.data.attributes.hp.value))
+
         };
 
-        if (data.hpChange >= 12 + data.actorConMod) {
-            let woundRiskCounter = actor.getFlag(moduleName, 'woundRiskCounter')
-                if (!woundRiskCounter) {
-                    woundRiskCounter = 1 
-                } else { 
-                    woundRiskCounter ++
-                }
+        let woundRiskCounter;
 
+        //effectively cause automatic fail due to massive damage 
+        if (data.hpChange >= 2 * (12 + data.actorConMod)) {
+            woundRiskCounter = 20
             return actor.setFlag(moduleName, 'woundRiskCounter', woundRiskCounter)
-        }}
+        } else if (data.hpChange >= 12 + data.actorConMod) {
+            woundRiskCounter = actor.getFlag(moduleName, 'woundRiskCounter') 
+            if (!woundRiskCounter) {
+                woundRiskCounter = 1 
+            } else { 
+                woundRiskCounter ++
+            };
+            return actor.setFlag(moduleName, 'woundRiskCounter', woundRiskCounter);
+        }
+    }
 
     static async drawWound(actor) {
         const saveTest = 12 + (2 * actor.getFlag(moduleName, 'woundRiskCounter'));
         let wSave = await new Roll('d20').evaluate();
+        await wSave.toMessage();
+        
         let actorName = actor.data.name.capitalize()
         
         actor.unsetFlag(moduleName, 'woundRiskCounter')
         
         if (wSave.total < saveTest) {
-            ChatMessage.create({
+            if (wSave.total + 9 < saveTest) {
+                injuries.critWoundFailCalc(actor, actorName, saveTest);
+    
+            } else {ChatMessage.create({
                 content: wounds.format("gandg.GreatWoundDialogFailMessage", {
                     actorName: actorName,
                     gwFeatureName: 'wound',
+                    saveTest: saveTest,
                 }),
-            });
+            })};
             //create active effect
             await game.dfreds.effectInterface.addEffect({effectName: 'Open Wound', uuid: actor.uuid })
             await game.dfreds.effectInterface.addEffect({effectName: 'Bleeding', uuid: actor.uuid })
+            
+            let openWoundCount = actor.data.effects.filter(i => i.data.label === 'Open Wound').length
+            
+            if (openWoundCount > Math.ceil(actor.data.data.details.level/2) + actor.data.data.abilities.con.mod) {
+                await game.dfreds.effectInterface.addEffect({effectName: 'Unconscious', uuid: actor.uuid, overlay: true})
+                injuries.addInjuryToken(actor);
+            };
+            
         } else {
             ChatMessage.create({
                 content: wounds.format("gandg.GreatWoundDialogSuccessMessage", {
                     actorName: actorName,
                     gwFeatureName: 'wound',
+                    saveTest: saveTest,
                 }),
             });
         }
-
     }
 
     static woundSocket(socketData) {
@@ -155,15 +174,105 @@ class wounds {
     }
 }
 class injuries {
-    /*
-    trigger for falling from wounds
-    injury triggers
-    unconscious due to wounds or damage
-    fail wound confirmation check by 10 or more
-    suffer critical hit and attacker makes a second attack roll
-    */
-}   
 
+    static critWoundFailCalc(actor, actorName, saveTest) {
+        ChatMessage.create({
+            content: wounds.format("gandg.GreatWoundDialogCritFailMessage",{
+                actorName: actorName,
+                gwFeatureName: 'wound',
+                saveTest: saveTest,
+            }),  
+        });
+        //injury token due to critical wound fail
+        injuries.addInjuryToken(actor);
+    }
+    
+    static addInjuryToken(actor) {
+        let injuryToken = actor.getFlag(moduleName, 'injuryToken') 
+            if (!injuryToken) {
+                injuryToken = 1
+                return actor.setFlag(moduleName, 'injuryToken', injuryToken);
+            } else { 
+                injuryToken ++
+                return actor.setFlag(moduleName, 'injuryToken', injuryToken);
+            };
+    }   
+
+    static critAttack(workflow) {
+        if (workflow.isCritical && !workflow.actor.hasPlayerOwner) {
+            let targetArray = Array.from(workflow.targets)
+            let actor = workflow.actor
+            injuries.critCalculation(workflow, targetArray, actor)
+        }
+        
+    }
+
+    static async critCalculation(workflow, targetArray, actor) {
+        if(game.user.data.role !== 4 || actor.hasPlayerOwner) return
+
+        const attackRoll = workflow.attackRoll.formula;
+        new Dialog({
+            title: wounds.format("gandg.critInjuryDialogTitle", { gwFeatureName: 'crit injury', NPCName: actor.name }),
+            content: wounds.format("gandg.critInjuryDialogContents"),
+            buttons: {
+                one: {
+                    label: wounds.localize("gandg.Default_roll"),
+                    callback: () => {
+                        /** draw locally if we are the one prompting the change OR if not owned by any players */
+                            injuries.critConfirmation(attackRoll, targetArray)
+                    }
+                },
+                two: {
+                    label: wounds.localize("gandg.Default_critrollcancel"),
+                    click: () => {}
+                }
+            }
+        }).render(true)
+    }
+
+    static async critConfirmation(attackRoll, targetArray) {
+
+        let critConfAttack = await new Roll(attackRoll).evaluate()
+        let critConfValue = critConfAttack.total;
+        await critConfAttack.toMessage();
+        let impactedActors = []
+        for (let target of targetArray) {
+            if (critConfValue >= target.document._actor.data.data.attributes.ac.value) {
+                impactedActors.push(target.data.name.capitalize());
+                let targetActor = game.actors.get(target.data.actorId);
+                injuries.addInjuryToken(targetActor);
+            }
+        } 
+
+        let actorNames = impactedActors.join(", ")
+        if (actorNames.length > 0) {
+            ChatMessage.create({
+                content: wounds.format("gandg.critResults", {
+                    actorNames: actorNames,
+                    gwFeatureName: 'wound',
+                    saveTest: critConfValue,
+                    }),
+                });
+            }
+        }
+
+    static injuryAdjudication(combat) {
+        let injuredActors = []
+        combat.combatants.forEach(combatant => {
+            if (combatant.actor.data?.flags["gandg-combat"]?.injuryToken)
+            injuredActors.push(`${combatant.actor.data.name.capitalize()} with ${combatant.actor.data?.flags["gandg-combat"]?.injuryToken} injury tokens`)
+            return combatant.actor.unsetFlag(moduleName, 'injuryToken');
+        });  
+        let injuredActorMessage = injuredActors.join(" AND ")
+        if (injuredActorMessage.length > 0) {
+            ChatMessage.create({
+                content: wounds.format("gandg.injuryAdjudication", {
+                    injuredActors: injuredActorMessage
+                    }),
+                });
+            }
+    }
+} 
 
 
 
@@ -175,3 +284,4 @@ Hooks.once('init', async function() {
     console.log('gandg combat | initialize')
 
 });
+
